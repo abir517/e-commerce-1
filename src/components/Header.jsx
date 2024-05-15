5d050055d6fb120dd71bca86c8d3955d3a62d759
abir517 <talimabirhasan@gmail.com>
2024-05-15 19:56:03 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import  Container  from './Container'
+import React from 'react'
+
+const Header = () => {
+  return (
+ <container>
+  <div>
+    <h1 className='text-[red]'>hello</h1>
+  </div>
+   
+ </container>
+
+  )
+}
+
+export default Header
